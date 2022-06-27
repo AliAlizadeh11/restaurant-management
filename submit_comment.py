cecf875db60ed1ea180e4cd289fae38676fccdc2
AliAlizadeh <alializadeh.dev11@gmail.com>
2022-06-27 20:48:05 +0430

    submit customer's commets

---
+++ submit_comment.py
@@ -0,0 +1,10 @@
+class SubmitComment:
+    def __init__(self):
+        pass
+    
+    def get_and_show_comment(self):
+        get_comment_input = input('Please Enter your comment :)')
+    
+
+a = SubmitComment()
+a.get_and_show_comment()
