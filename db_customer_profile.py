c031cc1312b90fd2004c35d7e01fcd18103ef5ca
AliAlizadeh <alializadeh.dev11@gmail.com>
2022-07-06 11:17:51 +0430

    update customer files

--- db_customer_profile.py
+++ db_customer_profile.py
@@ -8,36 +8,36 @@ c = conn.cursor()
 
 
 def update_table_name(name, national_code):
-        """update records from customers"""
-        c.execute(f"UPDATE customers SET name = '{name}' WHERE national_code = '{national_code}'")
+        """update records from customer"""
+        c.execute(f"UPDATE customer SET name = '{name}' WHERE national_code = '{national_code}'")
         conn.commit()
 
 
 def update_table_family_name(family_name, national_code):
-        """update records from customers"""
-        c.execute(f"UPDATE customers SET family_name = '{family_name}' WHERE national_code = '{national_code}'")
+        """update records from customer"""
+        c.execute(f"UPDATE customer SET family_name = '{family_name}' WHERE national_code = '{national_code}'")
         conn.commit()
 
 
 def update_table_phone_number(phone_number, national_code):
-        """update records from customers"""
-        c.execute(f"UPDATE customers SET phone_number = '{phone_number}' WHERE national_code = '{national_code}'")
+        """update records from customer"""
+        c.execute(f"UPDATE customer SET phone_number = '{phone_number}' WHERE national_code = '{national_code}'")
         conn.commit()
 
 
 def update_table_email(email, national_code):
-        """update records from customers"""
-        c.execute(f"UPDATE customers SET email = '{email}' WHERE national_code = '{national_code}'")
+        """update records from customer"""
+        c.execute(f"UPDATE customer SET email = '{email}' WHERE national_code = '{national_code}'")
         conn.commit()
 
 
 def update_table_password(password, national_code):
-        """update records from customers"""
-        c.execute(f"UPDATE customers SET password = '{password}' WHERE national_code = '{national_code}'")
+        """update records from customer"""
+        c.execute(f"UPDATE customer SET password = '{password}' WHERE national_code = '{national_code}'")
         conn.commit()
 
 
 def update_table_photo(photo, national_code):
-        """update records from customers"""
-        c.execute(f"UPDATE customers SET photo = '{photo}' WHERE national_code = '{national_code}'")
+        """update records from customer"""
+        c.execute(f"UPDATE customer SET photo = '{photo}' WHERE national_code = '{national_code}'")
         conn.commit()
