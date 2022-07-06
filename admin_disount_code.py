73917726299a6aab837188f9566ae110d126f390
AliAlizadeh <alializadeh.dev11@gmail.com>
2022-07-06 22:38:03 +0430

    discount code added

--- admin_disount_code.py
+++ admin_disount_code.py
@@ -13,5 +13,5 @@ def discount_code(customer_email):
     smtplib_obj.starttls()
     
     smtplib_obj.login("AP4002Group4@gmail.com", "rginclqnytypwofj")
-    smtplib_obj.sendmail("AP4002Group4@gmail.com", f"{create_customer.CreateCustomer.email}", f"Subject:discount code\n{final_code}")
+    smtplib_obj.sendmail("AP4002Group4@gmail.com", f"{customer_email}", f"Subject:discount code\n{final_code}")
     smtplib_obj.quit()
