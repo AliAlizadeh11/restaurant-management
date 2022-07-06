fefbb4d38c38c75a28f6fd01ec1177fec3b4ac5c
AliAlizadeh <alializadeh.dev11@gmail.com>
2022-07-06 15:35:18 +0430

    admin login & cmahdi2000heck password

--- admin_login.py
+++ admin_login.py
@@ -4,13 +4,7 @@ class LoginAdmin:
         self.password = password
         
     def check_username_password(self):
-        if self.username == 'AP4002Group4@gmail.com' and self.password == 'alikousha_ap4002_group4':
+        if self.username == 'mahdihosseinizade2000@gmail.com' and self.password == 'mahdi2000':
             return 'You have successfully logged in'
         else:
             return 'Wrong username or Password, Please Try again'
-        
-# a = LoginAdmin('AP4002Group4@gmail.com', 'alikousha_ap4002_group4')
-# b = LoginAdmin('AP4002Group4@', 'alikousha_ap4002_group4')
-
-# print(a.check_username_password())
-# print(b.check_username_password())
