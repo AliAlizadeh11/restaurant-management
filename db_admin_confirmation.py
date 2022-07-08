70e244cd0f1bbe5a64388d9b9d66c4f3a555e1af
AliAlizadeh <alializadeh.dev11@gmail.com>
2022-07-08 08:39:35 +0430

    database admin confirmation added

---
+++ db_admin_confirmation.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+#connect to database
+conn = sqlite3.connect('admin_confirmation.db')
+
+#create a cursor
+c = conn.cursor()
+
+
+def create_table():
+        """create a table"""
+        c.execute('''CREATE TABLE IF NOT EXISTS admin_confirmation
+                (foods text NOT NULL, 
+        )''')
+
+#information = [food1, food2, food3, ...] ----> list must be without tuple
+def insert_table(information):
+        """insert into admin_confirmation table"""
+        c.execute("INSERT INTO admin_confirmation VALUES (?)", information)
+        conn.commit()
